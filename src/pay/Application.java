9e5fefc85ee96eadabac3ed1f2c919a839543694
fk <fk@ubuntu.ubuntu-domain>
2019-10-29 20:10:37 +0800

    init

---
+++ src/pay/Application.java
@@ -0,0 +1,10 @@
+package pay;
+
+import java.net.http.HttpClient;
+
+public class Application {
+	HttpClient client = HttpClient.newHttpClient();
+	public static void main(String[] args) {
+		
+	}
+}
